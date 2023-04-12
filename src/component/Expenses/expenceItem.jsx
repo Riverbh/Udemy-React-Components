2fbde590ee56ffd99717c412369d844a81166939
River Hansen <riverbh01@gmail.com>
2023-04-12 17:23:10 -0600

    added a drop down of the years

--- src/component/Expenses/expenceItem.jsx
+++ src/component/Expenses/expenceItem.jsx
@@ -6,22 +6,14 @@ import Card from '../UI/Card';
 
 
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title);
-    console.log("ExpenseItem Evaluated in React");
-
-    const clickHandler = () => {
-        setTitle('Updated!');
-        console.log(title);
-    };
-
+    
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 };
